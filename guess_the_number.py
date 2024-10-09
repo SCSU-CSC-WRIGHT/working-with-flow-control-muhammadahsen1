c1540e0b3f10d5df79a7acdfb787d85724f0a98d
muhammadahsen1 <160660355+muhammadahsen1@users.noreply.github.com>
2024-10-09 16:33:23 -0400

    Create guess_the_number.py
    guess the numbers

---
+++ guess_the_number.py
@@ -0,0 +1,28 @@
+#    import random
+# Step 1: Generate a random number between 1 and 10
+#number = random.randint(1,10)
+
+# Step 2: Set the number of attempts
+#attempts = 3
+
+# Step 3: Loop to allow the user to guess up to 3 times
+#while attempts > 0:
+    # Step 4: Get the user's guess
+   # guess = int(input("Guess the number (between 1 and 10): "))
+    
+    # Step 5: Compare the guess with the random number
+   # if guess == random_number:
+   #     print("Congratulations! You guessed the correct number.")
+    #    break
+   # elif guess < random_number:
+   #     print("Too low!")
+  #  else:
+   #     print("Too high!")
+    
+    # Step 6: Decrease the number of attempts
+    #attempts -= 1
+
+# Step 7: If the user runs out of attempts
+#if attempts == 0:
+  #  print(f"Sorry, you're out of attempts. The correct number was {random_number}.")\
+  
